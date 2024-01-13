448e7aac1f10769c3f5fcd519b87d8f44fcada23
Joy Ghosh <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-13 14:29:23 +0000

    digital-live-clock

---
+++ project-3/codewithjoy.js
@@ -0,0 +1,10 @@
+
+//const clock = document.getElementById('clock') // we can use this, or below one,i.e ,querySelector
+const clock = document.querySelector('#clock')
+
+setInterval(function () {
+
+    let date = new Date()
+    //console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+},1000) // 2000 for 2 seconds
